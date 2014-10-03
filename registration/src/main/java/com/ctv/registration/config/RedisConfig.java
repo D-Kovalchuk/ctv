fd7af99b9e2061cf15bedbba83d2f0dc4c629b6e
dimon <world.bin.info@gmail.com>
2014-10-03 20:59:25 +0300

    add config files

---
+++ registration/src/main/java/com/ctv/registration/config/RedisConfig.java
@@ -0,0 +1,49 @@
+package com.ctv.registration.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
+
+/**
+ * @author Dmitry Kovalchuk
+ */
+@Configuration
+public class RedisConfig {
+
+    @Value("${redis.maxInactiveInterval}")
+    private int maxInterval;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() throws Exception {
+        return new JedisConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, ExpiringSession> redisTemplate(RedisConnectionFactory connectionFactory) {
+        final RedisTemplate<String, ExpiringSession> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+
+    @Bean
+    public RedisOperationsSessionRepository sessionRepository(RedisTemplate<String, ExpiringSession> redisTemplate) {
+        final RedisOperationsSessionRepository redisOperationsSessionRepository = new RedisOperationsSessionRepository(redisTemplate);
+        redisOperationsSessionRepository.setDefaultMaxInactiveInterval(maxInterval);
+        return redisOperationsSessionRepository;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
---
+++ registration/src/main/java/com/ctv/registration/config/RegistrationConfig.java
@@ -0,0 +1,64 @@
+package com.ctv.registration.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.session.ExpiringSession;
+import org.springframework.session.SessionRepository;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.SessionRepositoryFilter;
+
+/**
+ * @author Dmitry Kovalchuk
+ */
+@Configuration
+@EnableWebMvcSecurity
+@EnableWebSecurity
+public class RegistrationConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private SessionRepository<? extends ExpiringSession> sessionRepository;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        final SessionRepositoryFilter sessionRepositoryFilter = new SessionRepositoryFilter<>(sessionRepository);
+        sessionRepositoryFilter.setHttpSessionStrategy(new HeaderHttpSessionStrategy());
+
+        http
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/api/plants/**").hasRole("ADMIN")
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilterBefore(sessionRepositoryFilter, ChannelProcessingFilter.class)
+            .csrf().disable();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
