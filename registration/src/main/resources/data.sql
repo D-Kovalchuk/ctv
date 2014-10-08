263df2389065c56b362f1d5fb4487976fabfcd3a
dimon <world.bin.info@gmail.com>
2014-10-09 02:22:39 +0300

    add insert for one more table change hhbm2ddl to create-drop

--- registration/src/main/resources/data.sql
+++ registration/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into users values (1, 'username@company.com', true, 'pw', 'company.com', 'ROLE_WATCHER', 'username')
+-- username : password
+insert into users values (1, 'username@company.com', true, '$2a$10$M8nZRjPQ0i8WSODkcHZ9kO0L/ekDPYyqeE9nofi6gDOd6oai8TjH.', 'company.com', 'ROLE_WATCHER', 'username');
+insert into authorities values (1, 'ROLE_ORGANIZER', 'username');
