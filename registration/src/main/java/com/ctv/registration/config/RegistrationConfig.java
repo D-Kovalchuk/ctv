3cae2a9f20ca990a5dfbc551e885ca0f0c8800f6
Timur Yarosh <ti.yarosh@gmail.com>
2014-10-10 20:43:14 +0300

    add log tracing

--- registration/src/main/java/com/ctv/registration/config/RegistrationConfig.java
+++ registration/src/main/java/com/ctv/registration/config/RegistrationConfig.java
@@ -1,6 +1,7 @@
 package com.ctv.registration.config;
 
 import com.ctv.config.converter.EnableConverters;
+import com.ctv.registration.LogTracer;
 import com.ctv.registration.core.RegistrationService;
 import com.ctv.registration.core.RegistrationServiceImpl;
 import com.ctv.registration.core.port.in.UserPersistenceAdapter;
@@ -8,8 +9,14 @@ import com.ctv.registration.persistence.adapter.UserPersistenceAdapterImpl;
 import com.ctv.registration.persistence.adapter.UserRepository;
 import com.ctv.registration.web.adapter.UserMvcAdapter;
 import com.ctv.registration.web.adapter.UserMvcAdapterImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.interceptor.CustomizableTraceInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -19,6 +26,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories(RegistrationConfig.PERSISTENCE_PACKAGE)
 @EnableConverters("com.ctv.registration.persistence.adapter.converter")
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class RegistrationConfig {
 
     public static final String PERSISTENCE_PACKAGE = "com.ctv.registration.persistence";
@@ -36,6 +44,22 @@ public class RegistrationConfig {
     @Bean
     public UserPersistenceAdapter registrationPersistenceAdapter(UserRepository userRepository, ConversionService conversionService) {
         return new UserPersistenceAdapterImpl(userRepository, conversionService);
+    }
+
+    @Bean
+    public CustomizableTraceInterceptor customizableTraceInterceptor() {
+        CustomizableTraceInterceptor customizableTraceInterceptor = new LogTracer(new ObjectMapper());
+        customizableTraceInterceptor.setUseDynamicLogger(true);
+        customizableTraceInterceptor.setEnterMessage("Entering $[targetClassShortName].$[methodName]($[arguments])");
+        customizableTraceInterceptor.setExitMessage("Leaving $[targetClassShortName].$[methodName](), returned $[returnValue]");
+        return customizableTraceInterceptor;
+    }
+
+    @Bean
+    public Advisor logAdvisor() {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("within(com.ctv..*) && !within(*.config..*)");
+        return new DefaultPointcutAdvisor(pointcut, customizableTraceInterceptor());
     }
 
 }
