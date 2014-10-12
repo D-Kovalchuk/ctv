bd423e0d79cf18920863ed0b78f3a535dc182323
Timur Yarosh <ti.yarosh@gmail.com>
2014-10-12 16:55:48 +0300

    add test for RegistrationService security

--- registration/core/src/test/java/com/ctv/registration/core/RegistrationServiceImplTest.groovy
+++ registration/core/src/test/java/com/ctv/registration/core/RegistrationServiceImplTest.groovy
@@ -3,11 +3,28 @@ package com.ctv.registration.core
 import com.ctv.registration.core.adapter.UserPersistenceAdapter
 import com.ctv.registration.core.exception.UserIdNotFoundException
 import com.ctv.registration.core.model.UserModel
+import com.ctv.security.config.client.CtvUserDetails
+import com.ctv.security.config.client.CtvUserDetailsBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.test.context.support.WithUserDetails
+import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
+
+import static org.mockito.Mockito.*
 
 /**
  * @author Timur Yarosh
  */
+@ContextConfiguration(classes = [CoreTestConfiguration])
 class RegistrationServiceImplTest extends Specification {
 
     static final String USERNAME = "username"
@@ -17,10 +34,14 @@ class RegistrationServiceImplTest extends Specification {
     static final String SITE = "http://company.com"
     static final int ID = 0
     static final boolean ENABLED = true
+    static final String ROLE = "USER"
 
-    def persistenceAdapterMock = Mock(UserPersistenceAdapter)
-    def registrationService = new RegistrationServiceImpl(persistenceAdapterMock)
     def userModel
+
+    @Autowired
+    RegistrationService registrationService
+    @Autowired
+    UserPersistenceAdapter persistenceAdapterMock
 
     void setup() {
         userModel = new UserModel()
@@ -33,6 +54,8 @@ class RegistrationServiceImplTest extends Specification {
             site = SITE
             enabled = ENABLED
         }
+
+        reset(persistenceAdapterMock)
     }
 
     def "should create new User"() {
@@ -40,24 +63,24 @@ class RegistrationServiceImplTest extends Specification {
         registrationService.createUser(userModel)
 
         then:
-        1 * persistenceAdapterMock.createUser(userModel)
+        verify(persistenceAdapterMock).createUser(userModel)
     }
 
     def "should delete existing user"() {
         given:
-        persistenceAdapterMock.findUserById(ID) >> userModel
+        when(persistenceAdapterMock.findUserById(ID)).thenReturn(userModel)
 
         when:
         registrationService.deleteUser(ID)
 
         then:
         !userModel.enabled
-        1 * persistenceAdapterMock.updateUser(userModel)
+        verify(persistenceAdapterMock).updateUser(userModel)
     }
 
     def "should throw UserIdNotFoundException if user with passed id not found"() {
         given:
-        persistenceAdapterMock.findUserById(ID) >> null
+        when(persistenceAdapterMock.findUserById(ID)).thenReturn(null)
 
         when:
         registrationService.deleteUser(ID)
@@ -66,20 +89,15 @@ class RegistrationServiceImplTest extends Specification {
         thrown(UserIdNotFoundException)
     }
 
-    def "should update existing user"() {
-        when:
-        registrationService.updateUser(userModel)
-
-        then:
-        1 * persistenceAdapterMock.updateUser(userModel)
-    }
-
     def "should find user by id"() {
+        given:
+        when(persistenceAdapterMock.findUserById(ID)).thenReturn(userModel)
+
         when:
         registrationService.findUserById(ID)
 
         then:
-        1 * persistenceAdapterMock.findUserById(ID) >> userModel
+        verify(persistenceAdapterMock).findUserById(ID) == null
     }
 
     def "should find all users"() {
@@ -91,6 +109,77 @@ class RegistrationServiceImplTest extends Specification {
         registrationService.findAllUsers(page, size)
 
         then:
-        1 * persistenceAdapterMock.findAllUsers(page, size)
+        verify(persistenceAdapterMock).findAllUsers(page, size) == null
     }
+
+    @WithUserDetails("username")
+    def "should fail if passed user id not equal to authenticated user id"() {
+        given:
+        userModel.with {
+            it.id = 12
+        }
+
+        when:
+        registrationService.updateUser(userModel)
+
+        then:
+        thrown(AccessDeniedException)
+    }
+
+    def "should fail if anonymous user request update"() {
+        when:
+        registrationService.updateUser(userModel)
+
+        then:
+        thrown(AuthenticationCredentialsNotFoundException)
+    }
+
+    @WithUserDetails("username")
+    def "should update passed user"() {
+        when:
+        registrationService.updateUser(userModel)
+
+        then:
+        verify(persistenceAdapterMock).updateUser(userModel)
+    }
+
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    @Configuration
+    static class CoreTestConfiguration {
+
+        @Bean
+        UserPersistenceAdapter userPersistenceAdapter() {
+            mock(UserPersistenceAdapter)
+        }
+
+        @Bean
+        RegistrationService registrationService(UserPersistenceAdapter userPersistenceAdapter) {
+            new RegistrationServiceImpl(userPersistenceAdapter)
+        }
+
+        @Autowired
+        void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService());
+        }
+
+        @Bean
+        UserDetailsService userDetailsService() {
+            new UserDetailsService() {
+                @Override
+                CtvUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                    return CtvUserDetailsBuilder.get()
+                            .withUsername(USERNAME)
+                            .withPassword(PASSWORD)
+                            .withAuthorities(AuthorityUtils.createAuthorityList(ROLE))
+                            .withId(ID)
+                            .withEmail(EMAIL)
+                            .withType(TYPE)
+                            .withSite(SITE)
+                            .build();
+                }
+            }
+        }
+
+    }
+
 }
