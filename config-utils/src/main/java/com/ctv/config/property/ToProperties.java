a4b27bb179601d0a3c95d1b059988b9f22e75d81
dimon <world.bin.info@gmail.com>
2014-10-16 15:15:11 +0300

    fix ToProperties test

--- config-utils/src/main/java/com/ctv/config/property/ToProperties.java
+++ config-utils/src/main/java/com/ctv/config/property/ToProperties.java
@@ -1,15 +1,17 @@
 package com.ctv.config.property;
 
-import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 
 import java.lang.reflect.Field;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Properties;
 
+import static java.util.Objects.nonNull;
 import static java.util.stream.Collectors.toMap;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 import static org.apache.commons.lang3.reflect.FieldUtils.getAllFieldsList;
 import static org.apache.commons.lang3.reflect.FieldUtils.readField;
 
@@ -19,6 +21,7 @@ import static org.apache.commons.lang3.reflect.FieldUtils.readField;
 public class ToProperties {
 
     public static final String PROPERTY_REGEXP = "[\\$\\{\\}]";
+    private static final Logger log = LoggerFactory.getLogger(ToProperties.class);
 
     public Properties toProperties() {
         Map<String, String> propertiesMap = getAllFieldsList(getClass()).stream()
@@ -31,13 +34,17 @@ public class ToProperties {
     private boolean notBlank(Field field) {
         try {
             Object value = readField(field, this, true);
-            if (Objects.nonNull(value)) {
-                return StringUtils.isNotEmpty((CharSequence) value);
+            if (nonNull(value)) {
+                if (value instanceof String) {
+                    return isNotEmpty((CharSequence) value);
+                } else {
+                    return true;
+                }
             }
-            return false;
         } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+            // never happen
         }
+        return false;
     }
 
     private Properties asProperties(Map<String, String> propertiesMap) {
--- config-utils/src/test/java/com/ctv/config/ToPropertiesTest.groovy
+++ config-utils/src/test/java/com/ctv/config/ToPropertiesTest.groovy
@@ -1,29 +1,95 @@
 package com.ctv.config
-
 import com.ctv.config.property.ToProperties
 import org.springframework.beans.factory.annotation.Value
-import spock.lang.Ignore
 import spock.lang.Specification
 import spock.lang.Unroll
 
 /**
  * @author Dmitry Kovalchuk
  */
-@Ignore
 class ToPropertiesTest extends Specification {
 
     @Unroll
     def "should create Properties instance with one property where key = #key and value = #value"() {
-        setup: "a new PropertyHolder created"
+        given:
         def propertyHolder = new PropertyHolder(value as int)
+
+        when:
         def properties = propertyHolder.toProperties()
 
-        expect: "Properties containing one entry with key apples and value 10"
+        then: "Properties containing one entry with key apples and value 10"
         properties.getProperty(key) == value
 
         where:
         key      | value
         "apples" | "10"
+    }
+
+    def "IntegerPropertyHolder when property is set"() {
+        given:
+        def integerHolder = new IntegerPropertyHolder(10)
+
+        when:
+        def properties = integerHolder.toProperties()
+
+        then:
+        properties.getProperty("apples") as Integer == 10
+    }
+
+    def "IntegerPropertyHolder when property is not set"() {
+        given:
+        def integerHolder = new IntegerPropertyHolder(null)
+
+        when:
+        def properties = integerHolder.toProperties()
+
+        then:
+        properties.isEmpty()
+    }
+
+    def "StringPropertyHolder when property exists but value is not set"() {
+        given:
+        def stringHolder = new StringPropertyHolder("")
+
+        when:
+        def properties = stringHolder.toProperties()
+
+        then:
+        properties.isEmpty()
+    }
+
+    def "StringPropertyHolder when property does not exist"() {
+        given:
+        def stringHolder = new StringPropertyHolder(null)
+
+        when:
+        def properties = stringHolder.toProperties()
+
+        then:
+        properties.isEmpty()
+    }
+
+
+    private class StringPropertyHolder extends ToProperties {
+
+        @Value('${apples}')
+        private String property
+
+        StringPropertyHolder(String property) {
+            this.property = property
+        }
+
+    }
+
+    private class IntegerPropertyHolder extends ToProperties {
+
+        @Value('${apples}')
+        private Integer property
+
+        IntegerPropertyHolder(Integer property) {
+            this.property = property
+        }
+
     }
 
     private class PropertyHolder extends ToProperties {
@@ -33,8 +99,7 @@ class ToPropertiesTest extends Specification {
 
         PropertyHolder(int property) {
             this.property = property
-        };
-
+        }
     }
 
 }
