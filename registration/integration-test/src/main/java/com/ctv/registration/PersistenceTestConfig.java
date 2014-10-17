7d30efbc23eb4523915d73d1405a9a0c81a0ec92
dimon <world.bin.info@gmail.com>
2014-10-17 11:55:31 +0300

    add postgres plugin for dropping and creating test database

--- registration/integration-test/src/main/java/com/ctv/registration/PersistenceTestConfig.java
+++ registration/integration-test/src/main/java/com/ctv/registration/PersistenceTestConfig.java
@@ -1,13 +1,8 @@
 package com.ctv.registration;
 
 import com.ctv.registration.persistence.config.PersistenceConfig;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 import static com.ctv.registration.persistence.config.properties.PersistencePropertyConfig.PERSISTENCE_DEFAULT_PROPERTIES;
 
@@ -18,9 +13,4 @@ import static com.ctv.registration.persistence.config.properties.PersistenceProp
 @PropertySource("classpath:" + PERSISTENCE_DEFAULT_PROPERTIES)
 @PropertySource("classpath:persistence-test.properties")
 public class PersistenceTestConfig extends PersistenceConfig {
-
-    @Bean
-    public DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.HSQL).build();
-    }
 }
