b7053320dab2170bc64e48ed8a775d9594160377
dimon <world.bin.info@gmail.com>
2014-12-30 14:21:58 +0200

    add mongo dependencies

--- conference/conference-persistence/src/main/java/com/ctv/conference/persistence/SpringConferenceRepository.java
+++ conference/conference-persistence/src/main/java/com/ctv/conference/persistence/SpringConferenceRepository.java
@@ -2,12 +2,25 @@ package com.ctv.conference.persistence;
 
 import com.ctv.conference.persistence.adapter.ConferenceRepository;
 import com.ctv.conference.persistence.adapter.dto.ConferenceDto;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Repository;
 
 /**
  * @author Dmitry Kovalchuk
  */
 @Repository
-public interface SpringConferenceRepository extends ConferenceRepository,
-        org.springframework.data.repository.Repository<ConferenceDto, Integer> {
+public class SpringConferenceRepository implements ConferenceRepository {
+
+    private MongoOperations mongoOps;
+
+    public SpringConferenceRepository(MongoOperations mongoOps) {
+        this.mongoOps = mongoOps;
+    }
+
+    @Override
+    public ConferenceDto createConference(ConferenceDto conferenceDto) {
+        mongoOps.save(conferenceDto);
+        return conferenceDto;
+    }
+
 }
--- conference/conference-persistence/src/main/java/com/ctv/conference/persistence/config/PersistenceConfig.java
+++ conference/conference-persistence/src/main/java/com/ctv/conference/persistence/config/PersistenceConfig.java
@@ -1,13 +1,41 @@
 package com.ctv.conference.persistence.config;
 
 import com.ctv.conference.persistence.adapter.config.PersistenceAdapterConfig;
+import com.ctv.config.property.EnablePropertySource;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 
 /**
  * @author Dmitry Kovalchuk
  */
 @Configuration
+@EnablePropertySource
 @Import(PersistenceAdapterConfig.class)
-public class PersistenceConfig {
+@PropertySource("classpath:persistence-default.properties")
+@PropertySource(value = "file:${user.home}/.config/ctv/persistence.properties", ignoreResourceNotFound = true)
+public class PersistenceConfig extends AbstractMongoConfiguration {
+
+    @Value("${host}")
+    private String host;
+
+    @Value("${port}")
+    private int port;
+
+    @Value("${dbName}")
+    private String dbName;
+
+    @Override
+    protected String getDatabaseName() {
+        return dbName;
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient(host, port);
+    }
 }
